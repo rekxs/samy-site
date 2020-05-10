76e96f7e4409f622e8134eee62cd2bc16cd391e0
Milan Knab <m.knab@sepia.de>
2020-05-10 15:31:03 +0200

    add: current state

--- src/components/header.js
+++ src/components/header.js
@@ -1,42 +1,71 @@
 import { Link } from "gatsby"
-import PropTypes from "prop-types"
 import React from "react"
+import {useStaticQuery, graphql } from "gatsby";
+import Img from "gatsby-image";
+import logo from "../images/logo.svg";
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
-)
+const Header = () => {
 
-Header.propTypes = {
-  siteTitle: PropTypes.string,
+  const [state, setState] = React.useState(false);
+  const data = useStaticQuery(graphql`
+        query {
+          logo: file(relativePath: { eq: "logo_dog.png" }) {
+                childImageSharp {
+                    fluid(maxWidth: 300) {
+                        ...GatsbyImageSharpFluid
+                    }
+                }
+            }
+        }
+  `);
+
+  const menuActive = state ? 'is-active' : '';
+  const burgerActive = state ? 'is-active' : '';
+
+  function toggleMenu() {
+    console.log(state)
+    setState(!state)
+    console.log(state)
+  }
+
+  function clearMenu() {
+    setState(false)
+  }
+
+  return (
+    <header>
+      <nav className="navbar is-fixed-top" role="navigation" aria-label="main navigation">
+        <div className="navbar-brand">
+          <div className="navbar-item" onClick={clearMenu} >
+            <Link to="/">
+              Home
+            </Link>
+          </div>
+          <a role="button" onClick={toggleMenu} class={`navbar-burger burger ${burgerActive}`} aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+          </a>
+        </div>
+        <div className={`navigation navbar-menu ${menuActive}`} >
+          <div className="navbar-start">
+            <div className="navbar-item" onClick={toggleMenu}>
+              <Link to="/gallery">Gallery</Link>
+            </div>
+            <div className="navbar-item" onClick={toggleMenu}>
+              <Link to="/insta">Instagram</Link>
+            </div>
+            <div className="navbar-item" onClick={toggleMenu}>
+              <Link to="/about">About</Link>
+            </div>
+          </div>
+          <div className="navbar-end"></div>
+        </div>
+      </nav>
+    </header>
+  );
+
 }
 
-Header.defaultProps = {
-  siteTitle: ``,
-}
 
 export default Header
