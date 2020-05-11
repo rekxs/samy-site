c530ea1703eef6344a3050c71a4fe7fe8379b855
Milan Knab <m.knab@sepia.de>
2020-05-11 18:00:30 +0200

    add: new

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,38 @@
 import React from "react"
+import {useStaticQuery, graphql } from "gatsby";
+import Img from "gatsby-image";
 
+// Import Components
+import Header from '../components/header'
 
 const NotFoundPage = () => {
+  const data = useStaticQuery(graphql`
+      query {
+          image: file(relativePath: { eq: "404.jpg" }) {
+              childImageSharp {
+                  fluid(maxWidth: 300) {
+                      ...GatsbyImageSharpFluid
+                  }
+              }
+          }
+      }
+  `)
   return (
-    <div>
-      <p>404</p>    
+    <div class="notFound">
+      <Header />
+      <section class="hero is-fullheight-with-navbar">
+        <div class="hero-body">
+          <div class="container has-text-centered">
+            <div class="content-wrap">
+                <h1 class="">Error 404</h1>
+                <figure class="image">
+                    <Img fluid={data.image.childImageSharp.fluid} />
+                </figure>
+                <h2 class="">Sorry! This is not the Page you were looking for</h2>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   );
  }
