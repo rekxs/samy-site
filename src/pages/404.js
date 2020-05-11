f7654e676220bf6afdace672cb2e69dc1a2755c1
Milan Knab <m.knab@sepia.de>
2020-05-11 18:12:28 +0200

    add: load style on 404 refresh

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,7 +1,7 @@
 import React from "react"
 import {useStaticQuery, graphql } from "gatsby";
 import Img from "gatsby-image";
-
+import "../styles/styles.scss"
 // Import Components
 import Header from '../components/header'
 
